24164f7ab745e70c3a5a5550a518c5d19eb7f0c0
LudwigEffort <dr.feynman65@gmail.com>
2022-12-04 20:14:19 +0100

    add search methods and axios request

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,27 @@ export default {
     HeaderPage,
     MainPage,
   },
+  data() {
+    return {
+      baseApiUrl: 'https://api.themoviedb.org/3',
+      apiKey: 'b935f03551ba44af8b30664391480eca',
+      lang: 'en-US',
+      arrMovies: [],
+    };
+  },
   methods: {
     search(queryString) {
-
+      axios.get(`${this.baseApiUrl}/search/movie`, {
+        params: {
+          api_key: this.apiKey,
+          query: queryString,
+          language: this.lang,
+        },
+      })
+        .then((responseAxios) => {
+          this.arrMovies = responseAxios.data.results;
+          console.log(this.arrMovies);
+        });
     },
   },
 };
