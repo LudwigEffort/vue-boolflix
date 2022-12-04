ec49e349f3bbb67a55052bdab81fc607a55f9462
LudwigEffort <dr.feynman65@gmail.com>
2022-12-04 11:39:41 +0100

    add font awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue';
-import App from './App.vue';
+
 import 'bootstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronRight, faHatWizard } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import App from './App.vue';
+
+library.add(faChevronRight, faHatWizard);
+Vue.component('FontAwesomeIcon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
