6b0b97be5cc223baa1448922701478aefa36cc66
LudwigEffort <dr.feynman65@gmail.com>
2022-12-05 00:50:31 +0100

    add stars from font awesome

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 
 import 'bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronRight, faHatWizard } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faSolidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faStarHalfStroke } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue';
 
-library.add(faChevronRight, faHatWizard);
+library.add(faSolidStar, faStar, faStarHalfStroke);
 Vue.component('FontAwesomeIcon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
